b40b4a1f61557512810798af722228e394551781
Khanh Chung <khanh.d.chung@gmail.com>
2023-10-24 18:37:57 -0700

    Initial Commit

---
+++ Donix/DonixApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonixApp.swift
+//  Donix
+//
+//  Created by Khanh Chung on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct DonixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
