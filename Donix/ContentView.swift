f3c9192980f0c069ea370dbd880383729ef2ed25
Khanh Chung <khanh.d.chung@gmail.com>
2023-10-24 20:05:48 -0700

    design basic layout list of item

--- Donix/ContentView.swift
+++ Donix/ContentView.swift
@@ -7,15 +7,63 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Item: Identifiable, Hashable {
+    let name: String
+    let quantity: Int
+    let id = UUID()
+}
+
+struct ItemView: View {
+    let item: Item
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, Donix!")
+        HStack {
+            Text(item.name)
+            Spacer()
+            Text(String(item.quantity))
         }
-        .padding()
+    }
+}
+
+private var items = [
+    Item(name: "Sprinkle Vanila", quantity: 18),
+    Item(name: "Sprinkle Chocolate", quantity: 12),
+    Item(name: "Coconut Chocolate", quantity: 12),
+    Item(name: "Apple Fritter", quantity: 8)
+]
+
+struct ContentView: View {
+    @State private var searchText = ""
+    
+    var body: some View {
+        VStack() {
+            NavigationView {
+                List(items) { item in
+                    ItemView(item: item)
+                }
+                .navigationTitle("Donuts")
+                .searchable(text: $searchText)
+            }
+            
+            HStack {
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "square.and.pencil")
+                        .font(.title2)
+                        .imageScale(.large)
+                })
+                Spacer()
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "plus.app")
+                        .font(.title2)
+                        .imageScale(.large)
+                })
+            }
+            .padding(.horizontal)
+        }
     }
 }
 
